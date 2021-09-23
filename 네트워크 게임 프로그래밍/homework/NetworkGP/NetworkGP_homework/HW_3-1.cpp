6bdca08de313b5c768d2c589ada3d10bc554227b
윤sungzzuu <sungzzuu@gmail.com>
2021-09-23 14:36:50 +0900

    3주차 과제 수정

--- 네트워크 게임 프로그래밍/homework/NetworkGP/NetworkGP_homework/HW_3-1.cpp
+++ 네트워크 게임 프로그래밍/homework/NetworkGP/NetworkGP_homework/HW_3-1.cpp
@@ -5,12 +5,20 @@
 BOOL IsLittleEndian();
 BOOL IsBigEndian();
 
+int main()
+{
+	if (IsLittleEndian())
+		printf("시스템에서 사용하는 바이트 정렬방식은 리틀엔디안\n");
+	else
+		printf("시스템에서 사용하는 바이트 정렬방식은 빅엔디안\n");
+}
+
 BOOL IsLittleEndian()
 {
-	u_short d = 0x1234;
-	u_char* p = (u_char*)&d;
+	u_short d = 0x1234; // 2바이트 저장
+	u_char p = static_cast<u_char>(d); // 1바이트 저장
 
-	if (*p == 0x34)
+	if (p == 0x34)
 		return TRUE;
 	else
 		return FALSE;
