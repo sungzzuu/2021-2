aaa30299260284c99f2b11e5062dbc1e6595d261
YSJ <sungzzuu@gmail.com>
2021-11-15 18:30:13 +0900

    넷겜플 프로그레스바 추가

---
+++ 네트워크 게임 프로그래밍/homework/9.GUI/WinApp/WinApp/WinApp.cpp
@@ -0,0 +1,202 @@
+﻿#include <windows.h>
+#include <commctrl.h>
+#include <stdio.h>
+#include <UrlMon.h>
+#include <wininet.h>
+
+#pragma comment(lib,"urlmon.lib")
+#pragma comment(lib,"wininet.lib")
+
+int Pos;//프로그레스바 상태를 나타내는 변수
+
+//다운로드 상태 알아오기 위해 클래스 생성
+class StatusCallBack : public IBindStatusCallback
+{
+public:
+	virtual HRESULT STDMETHODCALLTYPE OnStartBinding(
+		/* [in] */ DWORD dwReserved,
+		/* [in] */ IBinding* pib) {
+		return S_OK;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE GetPriority(
+		/* [out] */ LONG* pnPriority) {
+		return S_OK;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE OnLowResource(
+		/* [in] */ DWORD reserved) {
+		return S_OK;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE OnProgress(
+		/* [in] */ ULONG ulProgress,
+		/* [in] */ ULONG ulProgressMax,
+		/* [in] */ ULONG ulStatusCode,
+		/* [in] */ LPCWSTR szStatusText)
+	{
+		if (ulProgressMax > 0)
+			Pos = (double)ulProgress / (double)ulProgressMax * 100.0;//얼마나 다운로드 했는지   
+
+		return S_OK;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE OnStopBinding(
+		/* [in] */ HRESULT hresult,
+		/* [unique][in] */ LPCWSTR szError) {
+		return S_OK;
+	}
+
+
+	virtual /* [local] */ HRESULT STDMETHODCALLTYPE GetBindInfo(
+		/* [out] */ DWORD* grfBINDF,
+		/* [unique][out][in] */ BINDINFO* pbindinfo) {
+		return S_OK;
+	}
+
+	virtual /* [local] */ HRESULT STDMETHODCALLTYPE OnDataAvailable(
+		/* [in] */ DWORD grfBSCF,
+		/* [in] */ DWORD dwSize,
+		/* [in] */ FORMATETC* pformatetc,
+		/* [in] */ STGMEDIUM* pstgmed) {
+		return S_OK;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE OnObjectAvailable(
+		/* [in] */ REFIID riid,
+		/* [iid_is][in] */ IUnknown* punk) {
+		return S_OK;
+	}
+
+	STDMETHOD_(ULONG, AddRef)() { return 0; }
+	STDMETHOD_(ULONG, Release)() { return 0; }
+
+	STDMETHOD(QueryInterface)(
+		/* [in] */ REFIID riid,
+		/* [iid_is][out] */ void __RPC_FAR* __RPC_FAR* ppvObject) {
+		return E_NOTIMPL;
+	}
+
+public:
+	StatusCallBack()
+	{
+
+	}
+
+};
+
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("Test");
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR ipszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+
+	hWnd = CreateWindow(lpszClass, lpszClass,
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+
+	while (GetMessage(&Message, NULL, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+
+
+	return (int)Message.wParam;
+
+
+}
+
+
+DWORD threadId;
+HANDLE hThread;
+HRESULT fileDownLoad;
+HWND hProgress;
+
+DWORD WINAPI ThreaDownloadProc(LPVOID lpParameter)
+{
+	HWND hwnd = (HWND)lpParameter;
+
+	StatusCallBack callback;
+
+	DeleteUrlCacheEntry("https://www.youtube.com/watch?v=pfYxoxL9LdU&ab_channel=%EC%A7%84%EC%98%81%ED%98%B8");//캐쉬 지우기(항상 다시 받기)
+
+	HRESULT hr = URLDownloadToFile(
+		NULL,
+		"https://www.youtube.com/watch?v=pfYxoxL9LdU&ab_channel=%EC%A7%84%EC%98%81%ED%98%B8",
+		"test.mp4",
+		0,
+		&callback
+	);
+
+	KillTimer(hwnd, 0);
+
+	hThread = NULL;
+
+	return hr;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+
+		//프로그레스바 생성
+		hProgress = CreateWindow(PROGRESS_CLASS, NULL, WS_CHILD | WS_VISIBLE | WS_BORDER,
+			10, 10, 400, 30, hWnd, NULL, g_hInst, NULL);
+
+		SendMessage(hProgress, PBM_SETRANGE, 0, MAKELPARAM(0, 100));//프로그레스 초기화
+		SendMessage(hProgress, PBM_SETPOS, Pos, 0);//프로그레스 초기값
+
+		return 0;
+
+		//왼쪽 버튼 클릭하면 다운로드 시작
+	case WM_LBUTTONDOWN:
+		SetTimer(hWnd, 0, 100, NULL);
+
+		if (!hThread)
+		{
+			//스레드 생성
+			hThread = CreateThread(NULL, 0, ThreaDownloadProc,
+				(LPVOID)hWnd, 0, &threadId);
+		}
+		return 0;
+
+	case WM_TIMER:
+		SendMessage(hProgress, PBM_SETPOS, Pos, 0);//프로그레스바 값 갱신
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
---
+++ 네트워크 게임 프로그래밍/homework/HW6_GUI Server/HW6_GUI Server/GUIServer.cpp
@@ -0,0 +1,217 @@
+#define _CRT_SECURE_NO_WARNINGS         // 최신 VC++ 컴파일 시 경고 방지
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // 최신 VC++ 컴파일 시 경고 방지
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <commctrl.h>
+
+
+#define SERVERPORT 9000
+#define BUFSIZE    512
+
+// 윈도우 프로시저
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// 오류 출력 함수
+void err_quit(char* msg);
+void err_display(char* msg);
+// 소켓 통신 스레드 함수
+DWORD WINAPI ServerMain(LPVOID arg);
+DWORD WINAPI ProcessClient(LPVOID arg);
+
+HINSTANCE hInst; // 인스턴스 핸들
+HWND hEdit; // 편집 컨트롤
+CRITICAL_SECTION cs; // 임계 영역
+HWND hProgress;
+int percent;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    hInst = hInstance;
+    InitializeCriticalSection(&cs);
+
+    // 윈도우 클래스 등록
+    WNDCLASS wndclass;
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = "MyWndClass";
+    if (!RegisterClass(&wndclass)) return 1;
+
+    // 윈도우 생성
+    HWND hWnd = CreateWindow("MyWndClass", "TCP 서버", WS_OVERLAPPEDWINDOW,
+        0, 0, 600, 200, NULL, NULL, hInstance, NULL);
+    if (hWnd == NULL) return 1;
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    // 소켓 통신 스레드 생성
+    CreateThread(NULL, 0, ServerMain, NULL, 0, NULL);
+
+    // 메시지 루프
+    MSG msg;
+    while (GetMessage(&msg, 0, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    DeleteCriticalSection(&cs);
+    return msg.wParam;
+}
+
+// 윈도우 프로시저
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg) {
+    case WM_CREATE:
+        //프로그레스바 생성
+        hProgress = CreateWindow(PROGRESS_CLASS, NULL, WS_CHILD | WS_VISIBLE | WS_BORDER,
+            10, 10, 400, 30, hWnd, NULL, hInst, NULL);
+
+        SendMessage(hProgress, PBM_SETRANGE, 0, MAKELPARAM(0, 100));//프로그레스 초기화
+        SendMessage(hProgress, PBM_SETPOS, 20, 0);//프로그레스 초기값
+
+        return 0;
+    case WM_SIZE:
+        MoveWindow(hEdit, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
+        return 0;
+    case WM_SETFOCUS:
+        SetFocus(hEdit);
+        return 0;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+    MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+    LocalFree(lpMsgBuf);
+    exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(char* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+    LocalFree(lpMsgBuf);
+}
+
+// TCP 서버 시작 부분
+DWORD WINAPI ServerMain(LPVOID arg)
+{
+    int retval;
+
+    // 윈속 초기화
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return 1;
+
+    // socket()
+    SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+    // bind()
+    SOCKADDR_IN serveraddr;
+    ZeroMemory(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(SERVERPORT);
+    retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+    if (retval == SOCKET_ERROR) err_quit("bind()");
+
+    // listen()
+    retval = listen(listen_sock, SOMAXCONN);
+    if (retval == SOCKET_ERROR) err_quit("listen()");
+
+    // 데이터 통신에 사용할 변수
+    SOCKET client_sock;
+    SOCKADDR_IN clientaddr;
+    int addrlen;
+    HANDLE hThread;
+
+    while (1) {
+        // accept()
+        addrlen = sizeof(clientaddr);
+        client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+        if (client_sock == INVALID_SOCKET) {
+            err_display("accept()");
+            break;
+        }
+
+        // 스레드 생성
+        hThread = CreateThread(NULL, 0, ProcessClient,
+            (LPVOID)client_sock, 0, NULL);
+        if (hThread == NULL) { closesocket(client_sock); }
+        else { CloseHandle(hThread); }
+    }
+
+    // closesocket()
+    closesocket(listen_sock);
+
+    // 윈속 종료
+    WSACleanup();
+    return 0;
+}
+
+// 클라이언트와 데이터 통신
+DWORD WINAPI ProcessClient(LPVOID arg)
+{
+    SOCKET client_sock = (SOCKET)arg;
+    int retval;
+    SOCKADDR_IN clientaddr;
+    int addrlen;
+    char buf[BUFSIZE + 1];
+
+    // 클라이언트 정보 얻기
+    addrlen = sizeof(clientaddr);
+    getpeername(client_sock, (SOCKADDR*)&clientaddr, &addrlen);
+
+    while (1) {
+        // 데이터 받기
+        retval = recv(client_sock, buf, BUFSIZE, 0);
+        if (retval == SOCKET_ERROR) {
+            err_display("recv()");
+            break;
+        }
+        else if (retval == 0)
+            break;
+
+
+        // 데이터 보내기
+        retval = send(client_sock, buf, retval, 0);
+        if (retval == SOCKET_ERROR) {
+            err_display("send()");
+            break;
+        }
+    }
+
+    // closesocket()
+    closesocket(client_sock);
+    //DisplayText("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\r\n",
+    //    inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+    return 0;
+}
